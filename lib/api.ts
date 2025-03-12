9b0ac5e962a57b882e3d0dabec6965744b658086
Tai <hotai1806@gmail.com>
2025-03-12 18:54:24 +0700

    add credentials

--- lib/api.ts
+++ lib/api.ts
@@ -3,6 +3,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 const client = new ApolloClient({
   uri: "https://pvovb7pb.api.sanity.io/v2023-08-01/graphql/production/default", // Replace with your API endpoint
   cache: new InMemoryCache(),
+  credentials: "include",
 });
 
 export default client;
